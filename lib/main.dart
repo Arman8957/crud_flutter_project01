acd6d2bc27bffa4c74f3a5e34516450d8953bbe9
Md Arman <novicethought0@gmail.com>
2024-05-29 10:53:10 +0000

    Crud project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(app);
+}
+
+class CrudApp  extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ,
+    );
+  }
+}
---
+++ lib/product_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatefulWidget {
+  
+  const ProductListScreen({super.key});
+  @override 
+  State<ProductListScreen> createState() => _ProductListScreenState();
+  
+}
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Product List"),
+      ),
+          body: ListView.builder(
+              itemCount: ,
+              itemBuilder: (context, index) {
+
+          }),
+    );
+  }
+}
